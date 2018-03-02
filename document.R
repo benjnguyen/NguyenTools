5d71f0cb92117dd44230846a22c891c67aa556f8
Benjamin Nguyen <nguy2171@umn.edu>
2018-03-02 12:45:23 -0600

    Small help doc for github.

---
+++ document.R
@@ -0,0 +1,20 @@
+# This is a test file
+# Created a file name "myFirstRfile.benjamin.nguy2171@umn.edu.here.R"
+
+# In git bash or the terminal in R
+
+# git remote add origin https://github.com/benjnguyen/STAT3701Tutorial.git
+# git push -u origin master
+
+# Grab everything from your package file and insert it into the local git repo
+# Check everything that you would like to commit, push it the the cloud
+
+# To test if that this works appropriately, use the install_github function
+library(devtools)
+install_github("benjnguyen/STAT3701Tutorial")
+library(LastFirstTools)
+
+install_github("benjnguyen/STAT3701HW3")
+install_github("benjnguyen/NguyenTools")
+library(NguyenTools)
+
